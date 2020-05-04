c799476a4de4ed5ebcd8c7710e32cae57f401c72
William <william_6426@hotmail.com>
2020-05-03 20:22:08 -0500

    Reset button working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,30 +7,40 @@ let resets = document.querySelector("#reset-button")
 let resetNumber = document.querySelector("num-resets")
 let teamOneShoots = 0
 let teamTwoShoots = 0
-let totalResets = 0
+let teamOneGoals = document.querySelector("teamone-numgoals")
+let teamTwoGoals = document.querySelector("teamtwo-numgoals")
 teamoneShootButton.addEventListener("click", function() {
     teamoneShots.innerHTML = teamOneShoots += 1
-    var random = Math.random( ) * 10; 
-    if (random > 2){
-        return "1";
+    var random = Math.random( ) * 1; 
+    
+    if (random > .5 );{
+        teamOneGoals = + 1
      }
-     else{ 
-        return "0";
-     } 
     
     console.log(random)
 })
 teamtwoShootButton.addEventListener("click", function()  {
     teamtwoShots.innerHTML = teamTwoShoots += 1
-    var random = Math.random( ) * 10;
-    if (random > 1){
+    var random = Math.random( ) * 1;
+    if (random > .5){
 
     }
-    else{
-
-    }
+    
     console.log(random)
+
+    if (teamOneGoals > teamTwoGoals){
+           "Team 1 wins";}
+    if (teamOneGoals = teamTwoGoals){
+            "tie";}
+    else{
+          "Team 2 wins";}
+
+
 })
 resets.addEventListener("click", function(){
-    resetNumber.innerHTML = totalResets += 1
+  resets.innerHTML = teamOneGoals +=0
+  resets.innerHTML = teamTwoGoals +=0
+  resets.innerHTML = teamoneShots +=0
+  resets.innerHTML = teamtwoShots +=0
+  resets.innerHTML = 1
 })
