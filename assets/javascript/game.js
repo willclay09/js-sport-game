96b238de41dbcb03e1be383c4fe82b36ab88f14e
William <william_6426@hotmail.com>
2020-05-07 12:26:25 -0500

    finished game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,50 +1,56 @@
-let shotsTaken = document.querySelector(".goals")
+//let shotsTaken = document.querySelector(".goals")
 let teamoneShootButton = document.querySelector("#teamone-shoot-button");
 let teamtwoShootButton = document.querySelector("#teamtwo-shoot-button")
 let teamoneShots = document.querySelector("#teamone-numshots")
 let teamtwoShots = document.querySelector("#teamtwo-numshots")
 let resets = document.querySelector("#reset-button")
 let resetNumber = document.querySelector("#num-resets")
-let teamOneShoots = 0
-let teamTwoShoots = 0
-let tg
 let teamOneGoals = document.querySelector("#teamone-numgoals")
 let teamTwoGoals = document.querySelector("#teamtwo-numgoals")
+let teamOneShoots = 0
+let teamTwoShoots = 0
+let tg1 = 0
+let tg2 = 0
+let resetNum = 0
 teamoneShootButton.addEventListener("click", function() {
     teamoneShots.innerHTML = teamOneShoots ++ 
     
-   tg = Number(teamOneGoals.innerHTML)
+   
     if (Math.random() > .5 ){
-        tg ++
-        teamOneGoals.innerHTML = tg
+    
+        teamOneGoals.innerHTML = tg1 +=1
      }
     
-    console.log(tg)
+    console.log(tg1)
 })
 teamtwoShootButton.addEventListener("click", function()  {
     teamtwoShots.innerHTML = teamTwoShoots ++
     
-    tg = Number(teamTwoGoals.innerHTML)
+    
     if (Math.random() > .5){
-      tg ++
-      teamTwoGoals.innerHTML = tg
+    
+      teamTwoGoals.innerHTML = tg2 +=1
     }
     
-    console.log(tg)
+    console.log(tg2)
 
-    if (teamOneGoals > teamTwoGoals){
-           "Team 1 wins";}
-    if (teamOneGoals = teamTwoGoals){
-            "tie";}
-    else{
-          "Team 2 wins";}
+   
       
-})
+    })
 resets.addEventListener("click", function()
 {
-  teamOneGoals.innerHTML = resets = 0
-  teamTwoGoals.innerHTML = resets = 0
-  teamoneShots.innerHTML = resets = 0
-  teamtwoShots.innerHTML = resets = 0
-   resetNumber.innerHTML = resets =+ 1
-}) 
+   if (teamOneGoals > teamTwoGoals){
+    console.log("Team 1 wins");}
+else if (teamOneGoals === teamTwoGoals){
+     console.log("tie");}
+else{
+   console.log("Team 2 wins");}
+   
+  teamOneGoals.innerHTML = tg1 = 0
+  teamTwoGoals.innerHTML = tg2 = 0
+  teamoneShots.innerHTML = teamOneShoots = 0
+  teamtwoShots.innerHTML = teamTwoShoots = 0
+  resetNumber.innerHTML = resetNum  += 1
+
+  })
+ 
